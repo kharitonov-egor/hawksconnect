1eac72ee9a91a239ad88fb911f8f985a10da29b0
Egor Kharitonov <ega.khar@gmail.com>
2025-12-21 11:50:37 -0500

    feat: icon added, next svgs deleted

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image"
+
 export default function NavBar() {
     return (
         <div className="w-full flex items-center justify-center h-[75px]">
         <div className="w-[1200px] flex flex-row justify-between p-4 bg-gray-200 mt-3 rounded-xl">
-          <h2>Logo</h2>
+          <Image src='/icon.png' alt="HawksConnect Logo" width={35} height={35}/>
           <div className="flex flex-row gap-2">
             <h2>Contact</h2>
             <h2>Button</h2>
