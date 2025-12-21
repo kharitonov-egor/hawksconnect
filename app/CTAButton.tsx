f3e59b3d8861b5b1dc4e116008a693466e4797cd
Egor Kharitonov <ega.khar@gmail.com>
2025-12-21 12:06:56 -0500

    feat: shadcn button added, font fixed

---
+++ app/CTAButton.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button"
+
+export default function CTAButton () {
+    return (
+        <Button className="bg-[#001E60] hover:bg-[#06357A]">Try it now</Button>
+
+    )
+}
--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,24 @@
 import Image from "next/image"
+import CTAButton from "./CTAButton"
+import Link from "next/link"
 
 export default function NavBar() {
     return (
         <div className="w-full flex items-center justify-center h-[75px]">
-        <div className="w-[1200px] flex flex-row justify-between p-4 bg-gray-200 mt-3 rounded-xl">
-          <Image src='/icon.png' alt="HawksConnect Logo" width={35} height={35}/>
-          <div className="flex flex-row gap-2">
-            <h2>Contact</h2>
-            <h2>Button</h2>
+        <div className="w-[1200px] h-full flex flex-row justify-between items-center p-4 bg-gray-200 mt-5 rounded-md">
+          <div>
+            <Link href="/">
+              <Image src='/icon.png' alt="HawksConnect Logo" width={50} height={50}/>
+            </Link>
+          </div>
+
+          <div className="flex flex-row gap-5 items-center">
+
+            <h2 className="font-medium">Contact</h2>
+            <div>
+            <CTAButton/>
+            </div>
+
           </div>
         </div>
       </div>
